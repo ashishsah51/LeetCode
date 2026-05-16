7b96489027b709e724557364c08766e9dbfb3f6b
Ashish Sah <77042139+ashishsah51@users.noreply.github.com>
2026-05-16 18:47:53 +0530

    Time: 17 ms (97.3%), Space: 46.1 MB (5.19%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -5,12 +5,15 @@ class Solution {
         int[][] dp = new int[n][2];
         for(int i=0; i<n; i++) {
             for(int j=i-1; j>=0; j--) {
-                if(nums[i] > nums[j])
-                    dp[i][0] = Math.max(dp[i][0], dp[j][0]);
-            }
-            for(int j=i-1; j>=0; j--) {
-                if(nums[i] > nums[j] && dp[i][0] == dp[j][0])
-                    dp[i][1]+=dp[j][1];
+                if(nums[i] > nums[j]) {
+                    if(dp[i][0] < dp[j][0]) {
+                        dp[i][1]=dp[j][1];
+                        dp[i][0] = dp[j][0];
+                    }
+                    else if(dp[i][0] == dp[j][0]) {
+                        dp[i][1]+=dp[j][1];
+                    }
+                }
             }
             dp[i][0] += 1;
             dp[i][1] = Math.max(dp[i][1], 1);
