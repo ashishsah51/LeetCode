e121c5cc5861c3298a5640a7c61106b406b3911c
Ashish Sah <77042139+ashishsah51@users.noreply.github.com>
2026-07-19 13:03:56 +0530

    Time: 7 ms (6.08%), Space: 43.3 MB (8.14%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.java
@@ -2,28 +2,69 @@ class Solution {
     int idx;
     Set<Character> set;
     public String smallestSubsequence(String s) {
-        set = new HashSet<>();
 
-        for(char c : s.toCharArray()) {
-            set.add(c);
-        }
+        int[] frq = new int[26];
+        Stack<Character> stk = new Stack<>();
 
-        int uniqueCnt = set.size();
-
-        idx = 0;
-        int n=s.length();
-        char[] res = new char[uniqueCnt];
-
-        for(int i=0; i<uniqueCnt; i++) {
-            for(char c='a'; c<='z'; c++) {
-                if(set.contains(c) && canAdd(c, s, n)) {
-                    res[i] = c;
-                    set.remove(c);
-                    break;
-                }
+        for(char c : s.toCharArray()) frq[c-'a']++;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            if(map.containsKey(s.charAt(i))){
+                frq[s.charAt(i)-'a']--;
+                continue;
             }
+            // System.out.print(s.charAt(i)+" ");
+            while(!stk.isEmpty() && s.charAt(i)<=stk.peek() && frq[stk.peek()-'a']>0) {
+                // System.out.print(stk.peek()+" ");
+                char ch = stk.pop();
+                int val = map.get(ch);
+                if(val==1) map.remove(ch);
+                else map.put(ch, val-1);
+            }
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1); 
+            stk.push(s.charAt(i));
+           // System.out.println(stk.toString());
+            frq[s.charAt(i)-'a']--;
         }
-        return String.valueOf(res);
+        int n = stk.size();
+        int[][] arr = new int[26][2];
+        while(!stk.isEmpty()) {
+            char c = stk.pop();
+            arr[c-'a'][0] = c-'a';
+            arr[c-'a'][1] = n+2;
+            n--;  
+        }
+
+        Arrays.sort(arr, (a, b) -> a[1] - b[1]);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<26; i++) {
+            if(arr[i][1]!=0) {
+                sb.append((char)(arr[i][0]+'a'));
+            } 
+        }
+        return sb.toString();
+        // set = new HashSet<>();
+
+        // for(char c : s.toCharArray()) {
+        //     set.add(c);
+        // }
+
+        // int uniqueCnt = set.size();
+
+        // idx = 0;
+        // int n=s.length();
+        // char[] res = new char[uniqueCnt];
+
+        // for(int i=0; i<uniqueCnt; i++) {
+        //     for(char c='a'; c<='z'; c++) {
+        //         if(set.contains(c) && canAdd(c, s, n)) {
+        //             res[i] = c;
+        //             set.remove(c);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return String.valueOf(res);
     }
 
     boolean canAdd(char c, String s, int n) {
